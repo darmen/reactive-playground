9445996dc5f41d1baa0faac577a9d85afae49d48
Darmen Amanbayev <ioxanssen@gmail.com>
2014-03-17 17:44:43 +0600

    Search Bar added

---
+++ Reactive Playground/DATwitterInstantViewControllerTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  DATwitterInstantViewControllerTableViewController.h
+//  Reactive Playground
+//
+//  Created by Darmen Amanbayev on 3/17/14.
+//  Copyright (c) 2014 Darmen Amanbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DATwitterInstantViewControllerTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
